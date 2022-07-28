0193399e88b04b2cb9e235b569cfa5fb040b8ece
kusumadoni77 <kusumadoni77@gmail.com>
2022-07-28 19:19:31 +0700

    edit Footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,7 +1,46 @@
-import React from "react";
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
 
-export default function Footer() {
-    return <div className="p-5 bg-blue-500 text-white text-3xl font-bold">
-        Footer Plants
-    </div>
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: '#0080FF',
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: 'white',
+}));
+
+function FormRow() {
+  return (
+    <React.Fragment>
+      <Grid item xs={4}>
+        <Item>Item</Item>
+      </Grid>
+      <Grid item xs={4}>
+        <Item>Item</Item>
+      </Grid>
+      <Grid item xs={4}>
+        <Item>Item</Item>
+      </Grid>
+    </React.Fragment>
+  );
+}
+
+export default function NestedGrid() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={1}>
+        <Grid container item spacing={3}>
+          <FormRow />
+        </Grid>
+        <Grid container item spacing={3}>
+          <FormRow />
+        </Grid>
+        <Grid container item spacing={3}>
+          <FormRow />
+        </Grid>
+      </Grid>
+    </Box>
+  );
 }
